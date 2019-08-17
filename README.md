61333f5a2a8368c37e4806bb0afa52ddbc80d8b7
Sebastian Hörl <sebastian.hoerl@ivt.baug.ethz.ch>
2019-08-17 14:31:49 +0200

    Set version to 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-The Python part of eqasim
+This repository contains the Python components of the eqasim framework.
+
+Currently, there is one main component, the Location Assignment preparation
+code, which is used in the respective scenario pipelines.
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="eqasim",
-    version="0.0.1",
+    version="1.0.0",
     author="Sebastian Hörl",
     author_email="sebastian.hoerl@ivt.baug.ethz.ch",
     description="Python part of eqasim",
